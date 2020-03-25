889aea8c1f5e265c85b0297038c41825e5265b51
Salih ŞAHİN <salihsahin@outlook.com>
2020-03-25 21:10:32 +0300

    Update ZeusContext.cs

--- Logfiks.DataAccess/Concrete/EntityFramework/ZeusContext.cs
+++ Logfiks.DataAccess/Concrete/EntityFramework/ZeusContext.cs
@@ -9,7 +9,7 @@ namespace Logfiks.DataAccess.Concrete.EntityFramework
     public class ZeusContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql("Host=185.120.6.139;Database=zeus;Username=zeus_logfiks;Password=Mr98521010*-_");
+        => optionsBuilder.UseNpgsql("Host=;Database=;Username=;Password=");
 
 
         public DbSet<TasitTipi> TasitTipi { get; set; }
